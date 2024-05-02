77acefed77030cb03505e9d1512d32930d82e6e8
Rameez Sajwani <rameezwazirali@hotmail.com>
2024-05-01 17:53:37 -0700

    Update welcome-message.sh

--- welcome-message.sh
+++ welcome-message.sh
@@ -3,8 +3,8 @@
 # Add preview banner to MOTD
 cat >> /etc/motd << EOF
 *******************************************************
-**            This VM was built from the:            **
-**      !! AZURE VM IMAGE BUILDER Custom Image !!    **
+**            This is custom OS for Orion            **
+**      !! Orion OS for Service Fabric v 1.0.0 !!    **
 **         You have just been Customized :-)         **
 *******************************************************
 EOF
